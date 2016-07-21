48be5964a6dbfc63a0d38e75a0b8a3a2f2d14335
hazrmard <ibrahim78786@gmail.com>
2016-07-21 12:37:23 -0500

    added parsing.h/.c + skip func

--- testing/reader.c
+++ testing/reader.c
@@ -5,16 +5,17 @@
 #include "utf8.h"
 #include "helpers.h"
 #include "io.h"
+#include "parsing.h"
 
 int main(int argc, char* argv[]) {
     setlocale(LC_ALL, "");
     printf("This program tests Urdu reading:\n");
     UBuffer b;
-    read_utf8_file("urdu.txt", &b);
+    long int sz = read_utf8_file("urdu.txt", &b);
     printf("Converted text:\n");
     int i=0;
     for (i=0; i<b.sz; i++) {
-        printf("%c", b.buff[i]);
+        printf("%C", b.buff[i]);
     }
     free(b.buff);
     return 0;
